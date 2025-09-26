ab66ba0fa2088b3fca5459c37589e155d991f3f0
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2025-09-26 13:58:58 +0900

    feat: __m68k_ctx_resume 関数の実装を追加

--- c.s
+++ c.s
@@ -10,8 +10,14 @@ _m68k_ctx_save:
 	movem.l	d0-d7/a0-a6,(4,a0)	* ctx->d[0..7] = D0-D7, ctx->a[0..6] = A0-A6
 	move.l	a7,(64,a0)		* ctx->a[7] = A7
 	move.w	sr,(68,a0)		* ctx->sr = SR
-	move.l	#0,d0			* return 0 (•Û‘¶‘¤)
+	moveq.l	#0,d0			* return 0 (•Û‘¶‘¤)
 	rts
 
 _m68k_ctx_resume:
-	rts
+	movea.l	(4,sp),a0		* a0.l = &ctx
+	move.l	(a0),d1			* d1.l = ctx->ret (ˆêŽž”ð“ï)
+	move.l	(64,a0),a7		* A7 = ctx->a[7]
+	movem.l	(12,a0),d2-d7/a0-a6	* ctx->d[2..7] = D2-D7, ctx->a[0..6] = A0-A6
+	moveq.l	#1,d0			* return 1 (•œ‹A‘¤)
+	movea.l	d1,a1			* a1.l = ctx->ret
+	jmp	(a1)			* •Û‘¶’n“_‚Ì‘±‚«‚Ö
